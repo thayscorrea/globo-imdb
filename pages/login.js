7da7ad4b8dd15321d6e470b3347ffcd336634d9f
Thays Lacerda <thayslcorrea3@gmail.com>
2024-05-02 21:23:19 -0300

    feat: add project and route login

---
+++ pages/login.js
@@ -0,0 +1,12 @@
+import Login from "../components/Login";
+import SeoHead from "../components/SeoHead";
+
+export default function Login() {
+    return (
+      <>
+        <SeoHead title='Login' />
+        <Login />
+      </>
+    );
+  }
+  
---
+++ pages/register.js
@@ -0,0 +1,12 @@
+import Register from "../components/Register";
+import SeoHead from "../components/SeoHead";
+
+export default function Register() {
+    return (
+      <>
+        <SeoHead title='Register user' />
+        <Register />
+      </>
+    );
+  }
+  
