269dcbb1e6e2daf25fc4ba91263f86b2a35d2fb5
Thays Lacerda <thays.lacerdac@gmail.com>
2024-05-06 19:51:44 -0300

    fix: clean project

--- components/SeoHead.js
+++ components/SeoHead.js
@@ -1,20 +1,21 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
 
+import Logo from '../public/favicon/icon.svg'
 
 // Default value for some meta data
 const defaultMeta = {
-  title: 'LaslesVPN',
-  siteName: 'LaslesVPN',
+  title: 'IMDb - Globo',
+  siteName: 'IMDbGlobo',
   description:
-    'Landing page VPN LaslesVPN Best VPN For Privacy, Country and Cheapest',
+    '',
   // change base url of your web (without '/' at the end)
-  url: 'https://next-landing-vpn.vercel.app',
+  url: '',
   type: 'website',
   robots: 'follow, index',
   // change with url of your image (recommended dimension = 1.91:1)
   // used in twitter, facebook, etc. card when link copied in tweet/status 
-  image: 'https://next-landing-vpn.vercel.app/assets/card-image.png',
+  image: Logo,
   author: 'Lorem Ipsum'
 };
 
@@ -82,10 +83,7 @@ const SeoHead = (props) => {
           />
         </>
       )}
-      {/* Favicons */}
-      {favicons.map((linkProps) => (
-        <link key={linkProps.href} {...linkProps} />
-      ))}
+      
       {/* Windows 8 app icon */}
       <meta name='msapplication-TileColor' content='#F53838' />
       <meta
@@ -97,91 +95,5 @@ const SeoHead = (props) => {
     </Head>
   );
 };
-
-// Favicons, other icons, and manifest definition
-const favicons = [
-  {
-    rel: 'apple-touch-icon',
-    sizes: '57x57',
-    href: '/favicon/apple-icon-57x57.png',
-  },
-  {
-    rel: 'apple-touch-icon',
-    sizes: '60x60',
-    href: '/favicon/apple-icon-60x60.png',
-  },
-  {
-    rel: 'apple-touch-icon',
-    sizes: '72x72',
-    href: '/favicon/apple-icon-72x72.png',
-  },
-  {
-    rel: 'apple-touch-icon',
-    sizes: '76x76',
-    href: '/favicon/apple-icon-76x76.png',
-  },
-  {
-    rel: 'apple-touch-icon',
-    sizes: '114x114',
-    href: '/favicon/apple-icon-114x114.png',
-  },
-  {
-    rel: 'apple-touch-icon',
-    sizes: '120x120',
-    href: '/favicon/apple-icon-120x120.png',
-  },
-  {
-    rel: 'apple-touch-icon',
-    sizes: '144x144',
-    href: '/favicon/apple-icon-144x144.png',
-  },
-  {
-    rel: 'apple-touch-icon',
-    sizes: '152x152',
-    href: '/favicon/apple-icon-152x152.png',
-  },
-  {
-    rel: 'apple-touch-icon',
-    sizes: '180x180',
-    href: '/favicon/apple-icon-180x180.png',
-  },
-  {
-    rel: 'mask-icon',
-    href: '/favicon/safari-pinned-tab.svg',
-    color: '#F59A9A',
-  },
-  {
-    rel: 'icon',
-    href: '/favicon/favicon.ico',
-  },
-  {
-    rel: 'icon',
-    type: 'image/png',
-    sizes: '16x16',
-    href: '/favicon/favicon-16x16.png',
-  },
-  {
-    rel: 'icon',
-    type: 'image/png',
-    sizes: '32x32',
-    href: '/favicon/favicon-32x32.png',
-  },
-  {
-    rel: 'icon',
-    type: 'image/png',
-    sizes: '96x96',
-    href: '/favicon/favicon-96x96.png',
-  },
-  {
-    rel: 'icon',
-    type: 'image/png',
-    sizes: '192x192',
-    href: '/favicon/android-icon-192x192.png',
-  },
-  {
-    rel: 'manifest',
-    href: '/site.webmanifest',
-  },
-];
 
 export default SeoHead;
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,12 @@
-import Feature from "../components/Feature";
 import Pricing from "../components/Pricing";
-import Hero from "../components/Hero";
 import Layout from "../components/Layout/Layout";
 import SeoHead from "../components/SeoHead";
 
 export default function Home() {
   return (
     <>
-      <SeoHead title='LaslesVPN Landing Page' />
+      <SeoHead title='Home' />
       <Layout>
-        <Hero />
-        <Feature />
         <Pricing />
       </Layout>
     </>
