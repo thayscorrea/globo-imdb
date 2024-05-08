b1f34900fc51d3cfc3d0f3743aa23fbde367b980
Thays Lacerda <thays.lacerdac@gmail.com>
2024-05-08 19:26:29 -0300

    fix: next-config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@ const nextConfig = {
     reactStrictMode: true,
     images : {
       domains : ['www.imdb.com', 'localhost']
-    }
+    },
+    pageExtensions: ['js'],
   }
   
   module.exports = nextConfig
